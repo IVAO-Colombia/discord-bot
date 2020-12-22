b95c231b641656deddb2ab542219e367b6edb49f
Jose Cuervo <manuelmuoz1221@outlook.com>
2020-12-21 20:09:42 -0500

    feat: llama

--- index.js
+++ index.js
@@ -8,19 +8,21 @@ client.on("ready", () => {
 
 let prefix = process.env.PREFIX;
 
-client.on("message", (message) => {
-  if (!message.content.startsWith(prefix) || !message.guild) return;
-  if (message.author.bot) return;
+if (message.content.startsWith(prefix + "inop")) {
+  // Checking if the message author is in a voice channel.
+  if (!message.member.voice.channel)
+    return message.reply("You must be in a voice channel.");
+  // Checking if the bot is in a voice channel.
+  if (message.guild.me.voice.channel)
+    return message.reply("I'm already playing.");
 
-  const cont = message.content.split(" ").slice(1);
-  const args = cont.join(" ");
-
-  if (message.content.startsWith(prefix + "ping")) {
-    message.channel.send("pong");
-  } else if (message.content.startsWith(prefix + "say")) {
-    if (!args) return;
-    message.channel.send(args);
-  }
-});
+  // Joining the channel and creating a VoiceConnection.
+  message.member.voice.channel.join().then((VoiceConnection) => {
+    // Playing the music, and, on finish, disconnecting the bot.
+    VoiceConnection.play("./audios/llamas/inoperante.mp3").on("finish", () =>
+      VoiceConnection.disconnect()
+    );
+  });
+}
 
 client.login(process.env.TOKEN);
