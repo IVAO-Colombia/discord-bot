476914bd65f0eded66d75f8573e31e508b6bc01e
Jose Cuervo <manuelmuoz1221@outlook.com>
2020-12-22 05:04:10 -0500

    fix: Real

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on("ready", () => {
-  client.user.setActivity("As Reas as It Gets", { type: "WATCHING" });
+  client.user.setActivity("As Real as It Gets", { type: "WATCHING" });
   console.log("Listo!");
 });
 
