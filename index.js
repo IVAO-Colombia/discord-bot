9febba6f0016f02284e8dc46bbc57edccc1b42f1
Jose Cuervo <manuelmuoz1221@outlook.com>
2020-12-21 20:13:19 -0500

    fix: fail

--- index.js
+++ index.js
@@ -7,22 +7,27 @@ client.on("ready", () => {
 });
 
 let prefix = process.env.PREFIX;
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || !message.guild) return;
+  if (message.author.bot) return;
+  const cont = message.content.split(" ").slice(1);
+  const args = cont.join(" ");
+  if (message.content.startsWith(prefix + "inop")) {
+    // Checking if the message author is in a voice channel.
+    if (!message.member.voice.channel)
+      return message.reply("You must be in a voice channel.");
+    // Checking if the bot is in a voice channel.
+    if (message.guild.me.voice.channel)
+      return message.reply("I'm already playing.");
 
-if (message.content.startsWith(prefix + "inop")) {
-  // Checking if the message author is in a voice channel.
-  if (!message.member.voice.channel)
-    return message.reply("You must be in a voice channel.");
-  // Checking if the bot is in a voice channel.
-  if (message.guild.me.voice.channel)
-    return message.reply("I'm already playing.");
-
-  // Joining the channel and creating a VoiceConnection.
-  message.member.voice.channel.join().then((VoiceConnection) => {
-    // Playing the music, and, on finish, disconnecting the bot.
-    VoiceConnection.play("./audios/llamas/inoperante.mp3").on("finish", () =>
-      VoiceConnection.disconnect()
-    );
-  });
-}
+    // Joining the channel and creating a VoiceConnection.
+    message.member.voice.channel.join().then((VoiceConnection) => {
+      // Playing the music, and, on finish, disconnecting the bot.
+      VoiceConnection.play("./audios/llamas/inoperante.mp3").on("finish", () =>
+        VoiceConnection.disconnect()
+      );
+    });
+  }
+});
 
 client.login(process.env.TOKEN);
