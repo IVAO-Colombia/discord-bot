2213701827575bf5a7fabde2f516b22810354fca
Jose Cuervo <59656796+21Insight@users.noreply.github.com>
2021-01-15 22:39:36 +0000

    fix: auds

--- index.js
+++ index.js
@@ -60,6 +60,22 @@ client.on("message", (message) => {
       );
     });
   }
+  if (message.content.startsWith(prefix + "gfrc")) {
+    // Checking if the message author is in a voice channel.
+    if (!message.member.voice.channel)
+      return message.reply("You must be in a voice channel.");
+    // Checking if the bot is in a voice channel.
+    if (message.guild.me.voice.channel)
+      return message.reply("I'm already playing.");
+
+    // Joining the channel and creating a VoiceConnection.
+    message.member.voice.channel.join().then((VoiceConnection) => {
+      // Playing the music, and, on finish, disconnecting the bot.
+      VoiceConnection.play("./audios/Gutenberg/gfrc.mp3").on("finish", () =>
+        VoiceConnection.disconnect()
+      );
+    });
+  }
   if (message.content.startsWith(prefix + "skinner")) {
     // Checking if the message author is in a voice channel.
     if (!message.member.voice.channel)
