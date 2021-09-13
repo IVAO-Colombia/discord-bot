d82b70ad4859e4b7993fc36e0cde4b6554856596
Jose Cuervo <59656796+21Insight@users.noreply.github.com>
2021-09-13 21:41:37 +0000

    feat: update auds

--- index.js
+++ index.js
@@ -47,6 +47,7 @@ client.on("message", (message) => {
         |fcura|
         |flive|
         |fpoli|
+        |yfufi|
         |rcop|
         |ridc|
         |rnhp|
@@ -75,6 +76,23 @@ client.on("message", (message) => {
     message.member.voice.channel.join().then((VoiceConnection) => {
       // Playing the music, and, on finish, disconnecting the bot.
       VoiceConnection.play("./audios/llamas/inoperante.mp3").on("finish", () =>
+        VoiceConnection.disconnect()
+      );
+    });
+  }
+
+  if (message.content.startsWith(prefix + "yfufi")) {
+    // Checking if the message author is in a voice channel.
+    if (!message.member.voice.channel)
+      return message.reply("You must be in a voice channel.");
+    // Checking if the bot is in a voice channel.
+    if (message.guild.me.voice.channel)
+      return message.reply("I'm already playing.");
+
+    // Joining the channel and creating a VoiceConnection.
+    message.member.voice.channel.join().then((VoiceConnection) => {
+      // Playing the music, and, on finish, disconnecting the bot.
+      VoiceConnection.play("./audios/Younz/yfufi.mp3").on("finish", () =>
         VoiceConnection.disconnect()
       );
     });
