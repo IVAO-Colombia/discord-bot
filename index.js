cc5629684f600584a8d386c76ea2f6ab401750a3
Jose Cuervo <59656796+21Insight@users.noreply.github.com>
2021-09-16 03:43:20 +0000

    feat: pedraza

--- index.js
+++ index.js
@@ -48,6 +48,7 @@ client.on("message", (message) => {
         |flive|
         |fpoli|
         |yfufi|
+        |drope|
         |rcop|
         |ridc|
         |rnhp|
@@ -93,6 +94,23 @@ client.on("message", (message) => {
     message.member.voice.channel.join().then((VoiceConnection) => {
       // Playing the music, and, on finish, disconnecting the bot.
       VoiceConnection.play("./audios/Younz/yfufi.mp3").on("finish", () =>
+        VoiceConnection.disconnect()
+      );
+    });
+  }
+
+  if (message.content.startsWith(prefix + "drope")) {
+    // Checking if the message author is in a voice channel.
+    if (!message.member.voice.channel)
+      return message.reply("You must be in a voice channel.");
+    // Checking if the bot is in a voice channel.
+    if (message.guild.me.voice.channel)
+      return message.reply("I'm already playing.");
+
+    // Joining the channel and creating a VoiceConnection.
+    message.member.voice.channel.join().then((VoiceConnection) => {
+      // Playing the music, and, on finish, disconnecting the bot.
+      VoiceConnection.play("./audios/pedraza/drope.mp3").on("finish", () =>
         VoiceConnection.disconnect()
       );
     });
